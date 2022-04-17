d9f6b9d2b2442c5e18e59de74fe500f02b0ad0c8
hardethanolninja <79213831+hardethanolninja@users.noreply.github.com>
2022-04-17 13:40:28 -0500

    added insta clone and updated home page

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "pages", "react_home")));
+app.use(express.static(path.join(__dirname, "pages", "instagram_clone")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 var path = require("path");
 
 router.get("/", function (req, res, next) {
-  res.render(path.join(__dirname, "pages", "react_home", "index.html"));
+  res.sendFile(path.join(__dirname, "../pages/react_home/index.html"));
+});
+
+router.get("/insta-clone", function (req, res, next) {
+  res.sendFile(path.resolve(__dirname, "../pages/instagram_clone/index.html"));
 });
 
 module.exports = router;
